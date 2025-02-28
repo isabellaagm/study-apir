3607832a2f1a2531802dd6c4acf58bf97548ed2a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:00:02 -0300

    feat: add Products

--- src/main/java/com/github/isabellaagm/study_apir/controller/ControllerPing.java
+++ src/main/java/com/github/isabellaagm/study_apir/controller/ControllerPing.java
@@ -1,4 +1,4 @@
-package com.github.isabellaagm.study_apir;
+package com.github.isabellaagm.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
